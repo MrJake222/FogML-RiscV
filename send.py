4dab05146afc1bd12672b9a31b820d84908d86eb
MrJake222 <norbertzpilicy@gmail.com>
2024-10-23 22:51:41 +0200

    Fixed hang on classification (send.py)

--- send.py
+++ send.py
@@ -7,27 +7,29 @@ import serial
 
 ser = serial.Serial('/dev/ttyACM2', baudrate=1000000)
 
-def prnline(prn=True, end="\n"):
+def getline(prn=True, end="\n"):
 	line = ser.readline().decode("utf-8").strip()
 	if prn:
 		print(line, end=end)
+	return line
 
 for i, l in enumerate(sys.stdin):
 	for x in l.strip().split():
 		x = float(x)
 		x = struct.pack("<f", x)
 		ser.write(x)
-	prnline(prn=False) # storing tick
+	getline(prn=False) # storing tick
 	
 	if (i+1) % 64 == 0:
-		prnline() # processing
-		prnline() # learning / classifying
+		getline() # processing
+		lc = getline() # learning / classifying
 		
 		start = time.time()
-		prnline() # DSP time
-		prnline() # RES time
-		prnline() # LOF time
-		prnline() # finished / lof score
+		if lc.startswith("learning"):
+			getline() # DSP time
+			getline() # RES time
+			getline() # LOF time
+		getline() # finished / lof score
 		end = time.time()
 		
 		print(f"[P {(i+1)//64:02d}] took: {end - start:.1f} sec")
